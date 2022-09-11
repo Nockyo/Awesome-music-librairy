7ea2cbc846c810e78321eb4f2f53e74a2596123f
Nockyo <95856198+Nockyo@users.noreply.github.com>
2022-09-11 18:06:40 +0200

    Set up React & React-redux

---
+++ react-music-library/src/features/counter/Counter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { decrement, increment } from "./counterSlice";
+import { useSelector, useDispatch } from "react-redux";;
+// import styles from '.Counter.module.css';
+
+export const Counter = () => {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <div>
+                <button
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <span>{count}</span>
+                <button
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
+        </div>
+    )
+}
